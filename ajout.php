0a9150f0d9a04fb13be4a9ba0fbc985ac618aa23
NoahParda <102357921+NoahParda@users.noreply.github.com>
2023-04-12 10:42:01 +0200

    update

---
+++ ajout.php
@@ -0,0 +1,43 @@
+<?php
+$errors = [];
+$data = [];
+
+if (empty($_POST['nom'])) {
+    $errors['nom'] = 'Le nom de la tache est nécessaire.';
+}
+
+if (empty($_POST['date'])) {
+    $errors['date'] = 'la date est nécessaire.';
+}
+
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
+
+//On essaie de se connecter
+try{
+    $mysqlClient = new PDO("mysql:host=localhost;dbname=gestion_de_tache", 'root', '');
+}
+//Permet de détecter des erreurs, en cas de non connection le code ne se lance pas. 
+catch(PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+    die;
+}
+function addtask($id,$budget,$itineraire,$date_livraison){
+    global $mysqlClient;
+    $sqlQuery1 = "INSERT INTO tache (nom, dates) VALUES ('$nom', '$date')";
+    $recipesStatement = $mysqlClient->prepare($sqlQuery1);
+    $recipesStatement->execute();
+  }
+
+$nom=$_POST['nom'];
+$date=$_POST['date'];
+addtask($nom, $date);
+?>
